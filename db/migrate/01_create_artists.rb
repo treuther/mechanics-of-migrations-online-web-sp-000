6e55da251306dccc3d65bec931e107a3a2299e14
Tracy Reuther <tracyreuther@gmail.com>
2019-08-19 18:30:10 +0000

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,7 +1,11 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
-  def up
+  
+  def change
+  create_table :artists do |t|
+    t.string :name
+    t.string :genre
+    t.integer :age
+    t.string :hometown
   end
- 
-  def down
-  end
+  
 end
